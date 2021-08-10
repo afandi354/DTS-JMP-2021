03cf8e1e83151d5420d1c91bd4024e7932ca7ba6
afandi354 <afandi.webmaster@gmail.com>
2021-08-10 16:18:58 +0700

    penambahan fitur kurang

--- AplikasiKalkulator/app/src/main/java/com/example/aplikasikalkulator/MainActivity.java
+++ AplikasiKalkulator/app/src/main/java/com/example/aplikasikalkulator/MainActivity.java
@@ -39,5 +39,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        kurang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((angka_pertama.getText().length() > 0) && (angka_kedua.getText().length()>0)){
+                    double angka1 = Double.parseDouble(angka_pertama.getText().toString());
+                    double angka2 = Double.parseDouble(angka_kedua.getText().toString());
+                    double result = angka1 - angka2;
+                    hasil.setText(Double.toString(result));
+                } else {
+                    Toast toast = Toast.makeText(MainActivity.this, "Mohon masukan angka pertama & kedua", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
     }
 }
