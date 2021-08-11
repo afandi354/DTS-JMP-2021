e4474655dfe45e3bf4948f509f53436d364eedc4
afandi354 <afandi.webmaster@gmail.com>
2021-08-11 19:44:00 +0700

    Add Catatan Harian

---
+++ Catatan Harian/app/src/main/java/kominfo/go/id/storage/proyek1/InsertViewActivity.java
@@ -0,0 +1,14 @@
+package kominfo.go.id.storage.proyek1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_view);
+    }
+}
